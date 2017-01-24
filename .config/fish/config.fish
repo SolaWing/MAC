c50759404b581d8c5fa40a2848fb361b2eb373b9
solawing <316786359@qq.com>
2017-01-24 09:16:23 +0800

    fzf pointer color. git use perl re

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ set -xg GREP_COLOR  '1;32'
 set -xg HISTCONTROL  ignoreboth:erasedups		# for 'ignoreboth': ignore duplicates and /^\s/
 set -xg HISTSIZE  9999
 set -xg JAVA_HOME  "/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home"
-set -xg FZF_DEFAULT_OPTS ' --bind alt-j:down,alt-k:up,alt-h:backward-char,alt-l:forward-char'
+set -xg FZF_DEFAULT_OPTS ' --bind="alt-j:down,alt-k:up,alt-h:backward-char,alt-l:forward-char,alt-space:jump,`:jump-accept" --color="pointer:15"'
 
 function fish_prompt
     set_color green
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -4,6 +4,7 @@ function fish_user_key_bindings
     bind \el forward-char
     bind \ej history-search-forward
     bind \ek history-search-backward
+    bind \c] forward-jump
     fzf_key_bindings
     bind \ee my_edit_line
 end
