34e8acb5dedec7e16ce3372f83b776a82916b81c
solawing <316786359@qq.com>
2018-12-29 16:44:00 +0800

    remove unuse code in xcpretty

--- bin/xcpretty.py
+++ bin/xcpretty.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import re, os, sys
-import shlex
 
 def echo(s):
     print(s, file=sys.stderr)
@@ -35,81 +34,6 @@ def read_until_empty_line(i):
 def extract_swift_files_from_swiftc(command):
     return [a for a in cmd_split(command) if a.endswith(".swift")]
 
-cache_dir = None
-async def extract_compile_command_from_swiftc(command, directory=None):
-    # from subprocess import Popen, PIPE, STDOUT
-    from asyncio.subprocess import create_subprocess_shell, PIPE, STDOUT
-    import json
-
-    # sp = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT, cwd=directory)
-    sp = await create_subprocess_shell(command, stdout=PIPE, stderr=STDOUT, cwd=directory)
-    items = []
-
-    if cache_dir is None:
-        # no cache_dir, don't replace filelist
-        _copy_filelist = lambda p: p
-    else:
-        # copy swiftc temp filelist
-        import shutil
-        copyed = set()
-        def _copy_filelist(op):
-            np = os.path.join(cache_dir, os.path.basename(op))
-            if op in copyed: return np
-            if os.path.exists(op):
-                shutil.copy2(op, np)
-                copyed.add(op)
-                return np
-            else:
-                echo(f"unexist filelist path: {op}")
-            return op
-
-    while True:
-      try:
-        # swiftc may echo a filelist, and delete it when end. so need to save the filelist when it's alive and can't read all..
-        line = await sp.stdout.readline() # type: bytes
-        if not line:
-            break # EOF
-
-        n = int(line) # ignore line non begin with a size number
-        if n <= 0: continue
-
-        # read(n)可能没读取n这么多字节
-        jsondata = await sp.stdout.readexactly(n)
-        info = json.loads(jsondata) # type: dict
-
-        cmd = info["command"]
-        cmdArgs = cmd_split(cmd)
-        if not cmdArgs[0].endswith("swift"): continue
-
-        primaryIndex = cmdArgs.index("-primary-file") + 1
-        primaryFile  = cmdArgs[primaryIndex]
-
-        def mayReplaceFileList(cmdArgs):
-            try: # replace filelist
-                fileListIndex = cmdArgs.index("-filelist") + 1
-                saveListPath      = _copy_filelist(cmdArgs[fileListIndex]) # type: list
-                cmdArgs[fileListIndex] = saveListPath
-            except ValueError:
-                pass
-            except Exception as e:
-                import traceback
-                traceback.print_exc()
-        mayReplaceFileList(cmdArgs)
-
-        item = {};
-        item["file"] = primaryFile
-        if directory: item["directory"] = directory
-        item["command"] = " ".join(map(shlex.quote, cmdArgs))
-        items.append(item)
-        echo("compile " + primaryFile)
-
-      except (KeyError, ValueError) as e: # info load exception, ignore no cmd, no primaryFile
-          pass
-      except Exception as e: # file load exception
-          import traceback
-          traceback.print_exc()
-    return items
-
 class XcodeLogParser(object):
     def __init__(self, _input, _logFunc):
         self._input = _input
@@ -133,11 +57,6 @@ class XcodeLogParser(object):
         module["command"] = command
         module["files"] = extract_swift_files_from_swiftc(command)
         return module
-
-        # 这个命令可以运行获得一堆命令, 输出到Stderr, 如果吐出-filelist, 结束时就会删除
-        ## 如果自己组装, 需要把里面的参数全部拆开
-        # 还是直接运行吧
-        # return extract_compile_command_from_swiftc(command, directory)
 
     def parse_compile_swift(self, line):
         m = compile_swift.match(line)
@@ -241,9 +160,6 @@ def main():
     else: in_fd = open(a.input, "r")
     if a.output == "-": get_out_fd = lambda: sys.stdout
     else:
-        global cache_dir;
-        cache_dir = os.path.join( os.path.dirname(os.path.abspath(a.output)), os.path.basename(a.output) + "cache")
-        os.makedirs(cache_dir, exist_ok=True)
         get_out_fd = lambda: open(a.output, "w") # open will clear file
 
     items = XcodeLogParser(in_fd, echo).parse()
