761a36f83b8f157681d46c552a82bf94e66f7c55
solawing <316786359@qq.com>
2019-02-02 11:45:47 +0800

    fzf use exact match default

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,7 @@ set -xg GREP_COLOR  '1;32'
 set -xg HISTCONTROL  ignoreboth:erasedups		# for 'ignoreboth': ignore duplicates and /^\s/
 set -xg HISTSIZE  9999
 set -xg JAVA_HOME  "/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home"
-set -xg FZF_DEFAULT_OPTS ' --bind="alt-j:down,alt-k:up,alt-h:backward-char,alt-l:forward-char,alt-space:jump,`:jump-accept" --color="pointer:15"'
+set -xg FZF_DEFAULT_OPTS ' --bind="alt-j:down,alt-k:up,alt-h:backward-char,alt-l:forward-char,alt-space:jump,`:jump-accept" --color="pointer:15" --exact'
 set -xg MANPAGER 'nvim +Man!'
 
 function fish_prompt
