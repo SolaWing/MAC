fb4b9833a4206ff984507c1bb89ebed380dc64ae
solawing <316786359@qq.com>
2019-06-26 12:32:59 +0800

    enable lint and flake8

--- bin/ios-profile.py
+++ bin/ios-profile.py
@@ -1,33 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os, re, argparse
 
-profileDir = os.path.expanduser("~/Library/MobileDevice/Provisioning Profiles")
+ProfileDir = os.path.expanduser("~/Library/MobileDevice/Provisioning Profiles")
+
 
 def parse():
     parser = argparse.ArgumentParser(description="find and print profile suitable for a device")
     parser.add_argument('identifier', help='device identifier')
     return parser.parse_args()
 
+
 def getContent(path):
     with open(path) as f:
         s = f.read()
     return s
 
-proFileNamePattern = re.compile(r'>Name<[^<]*<string>([^<]+)')
-def printProfile(filePair):
+
+ProFileNamePattern = re.compile(r'>Name<[^<]*<string>([^<]+)')
+
+
+def print_profile(filePair):
     identifer, content = filePair
-    m = proFileNamePattern.search(content)
-    if m: print identifer, ":\t", m.group(1)
-    else: print identifer
+    m = ProFileNamePattern.search(content)
+    if m: print(identifer, ":\t", m.group(1))
+    else: print(identifer)
+
 
 def main():
     args = parse()
-    os.chdir(profileDir)
+    os.chdir(ProfileDir)
     profiles = [f for f in os.listdir('.') if f.endswith('.mobileprovision')]
     pros = [(f, getContent(f)) for f in profiles]
     pros = [f for f in pros if args.identifier in f[1]]
     for var in pros:
-        printProfile(var)
+        print_profile(var)
+
 
 if __name__ == '__main__':
     main()
